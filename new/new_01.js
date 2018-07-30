08c56405c4bd5b33bf0e8b44e8fcf72c1173d740
2295790516@qq.com <2295790516@qq.com>
2018-07-30 17:37:17 +0800

    添加eslint

---
+++ new/new_01.js
@@ -0,0 +1,13 @@
+var foo = 1;
+var bar='a';
+
+var obj={ a : 1, b : 2};
+
+function fun() {
+    var aa=12;
+    console.log(aa);
+    return;
+}
+
+console.log(obj,bar,foo);
+fun();
---
+++ old/old_01.js
@@ -0,0 +1,8 @@
+var foo = 1;
+var bar=2;
+
+if (foo) {
+    bar = 1;
+} else {
+    bar = 2;
+}
