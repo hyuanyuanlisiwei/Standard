837f06bd099a2db43070c3111e49aca59f306ad8
2295790516@qq.com <2295790516@qq.com>
2018-08-02 10:31:56 +0800

    test hook

--- new/new_01.js
+++ new/new_01.js
@@ -1,17 +1,25 @@
-var foo = 1;
-var bar='a';
+let foo = 1;
+let bar='a';
 
-var obj={ a : 1, b : 2};
+let arr=[12,2,3,4];
+console.warn(arr);
+
+let obj={ a : 1, b : 2};
 
 function fun() {
-    var aa=12;
-    console.log(aa);
+    let aa=12;
+    console.warn(aa);
     return;
-
-
-    // console.log(aa);
 }
 
-console.log(obj,bar,foo);
-console.log(obj,bar,foo);
+console.warn(obj,bar,foo);
+console.warn(obj,bar,foo);
 fun();
+
+let reg=new RegExp('[]');
+console.warn(reg);
+
+// let obj2={a:1};
+// delete foo;
+
+console.warn(`${foo}`);
